382dfdd730fe25101421d63de5dd18975c5853e8
Daniel R Koris <korisd@gmail.com>
2019-10-27 19:14:03 -0600

    latest version of the code

---
+++ scripts/bakup.py
@@ -0,0 +1,228 @@
+# roup members: Andrew Seiler, Nick Elsasser, Justin Yun, Daniel Koris, Brendan Ostrom
+# CSCI 3302-101 - Intro to Robotics
+# Lab 4
+# Due 10/29/19
+
+import rospy
+import json
+import copy
+import time
+from geometry_msgs.msg import Pose2D
+from std_msgs.msg import Float32MultiArray, Empty, String, Int16
+import matplotlib.pyplot as plt
+import numpy as np
+from math import floor, sin, cos
+import matplotlib.animation as animation
+
+# GLOBALS
+pose2d_sparki_odometry = None  # Pose2D message object, contains x,y,theta members in meters and radians
+# TODO: Track servo angle in radians
+# TODO: Track IR sensor readings (there are five readings in the array: we've been using indices 1,2,3 for left/center/right)
+# TODO: Create data structure to hold map representation
+
+# TODO: Use these variables to hold your publishers and subscribers
+publisher_motor = None
+publisher_odom = None
+publisher_ping = None
+publisher_servo = None
+
+subscriber_odometry = None
+subscriber_state = None
+
+sensor_dict = {}  # 'servo', 'light_sensors', 'ping'
+
+SPARKI_SIZE = 5  # sparki is 5cmx5cm
+
+# MxN world grid
+# 1cm / 1cm grid size
+cell_size_x = 0.01
+cell_size_y = 0.01
+M = .42  # 42 cm in height
+N = .60  # 60 cm in width
+world_grid = np.zeros(shape=(int(M / cell_size_x), int(N / cell_size_y), 3))
+
+fig = plt.figure()
+img = plt.imshow(world_grid, interpolation='nearest')
+plt.draw()
+
+# CONSTANTS
+IR_THRESHOLD = 300  # IR sensor threshold for detecting black track. Change as necessary.
+CYCLE_TIME = 0.1  # In seconds
+
+
+def main():
+    global publisher_motor, publisher_ping, publisher_servo, publisher_odom
+    global IR_THRESHOLD, CYCLE_TIME
+    global pose2d_sparki_odometry
+
+    # TODO: Init your node to register it with the ROS core
+    rospy.init_node('Sp4rk1-C0ntr0ll3r', anonymous=True)
+    init()
+
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        # TODO: Implement CYCLE TIME
+
+        # TODO: Implement line following code here
+        #      To create a message for changing motor speed, use Float32MultiArray()
+        #      (e.g., msg = Float32MultiArray()     msg.data = [1.0,1.0]      publisher.pub(msg))
+
+        display_map()
+
+        ir = sensor_dict["light_sensors"]
+        motor_com = Float32MultiArray()
+
+        if ir[1] < IR_THRESHOLD:
+            motor_com.data = [-1.0, 1.0]
+
+        if ir[3] < IR_THRESHOLD:
+            motor_com.data = [1.0, -1.0]
+
+            # if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
+        if ir[2] < IR_THRESHOLD and ir[1] > IR_THRESHOLD and ir[3] > IR_THRESHOLD:
+            motor_com.data = [1.0, 1.0]
+
+        publisher_motor.publish(motor_com)
+        # TODO: Implement loop closure here
+        if False:
+            rospy.loginfo("Loop Closure Triggered")
+
+        # TODO: Implement CYCLE TIME
+        r.sleep()
+
+
+def updateFig(*args):
+    # update sparki position on map
+    for j in range(int(N / cell_size_y)):
+        for i in range(int(M / cell_size_x)):
+            if ((world_grid[i, j] == [0, 0, 0]).all() or (world_grid[i, j] == [255, 0, 0]).all()):
+                if (floor(pose2d_sparki_odometry.x * 100) == i and floor(pose2d_sparki_odometry.y * 100) == j):
+                    world_grid[i, j] = [255, 0, 0]
+                else:
+                    world_grid[i, j] = [0, 0, 0]
+    img.set_data(world_grid)
+    plt.pause(0.00001)
+    return [img]
+
+
+def init():
+    global publisher_motor, publisher_ping, publisher_servo, publisher_odom
+    global subscriber_odometry, subscriber_state
+    global pose2d_sparki_odometry
+
+    # publishers
+    publisher_motor = rospy.Publisher('/sparki/motor_command', Float32MultiArray)
+    publisher_ping = rospy.Publisher('/sparki/ping_command', Int16)
+    publisher_odom = rospy.Publisher('/sparki/set_odometry', Pose2D)
+    publisher_servo = rospy.Publisher('/sparki/set_servo', Int16)
+
+    # subscribers
+    subscriber_odometry = rospy.Subscriber('/sparki/odometry', Pose2D, callback_update_odometry)
+    subscriber_state = rospy.Subscriber('/sparki/state', String, callback_update_state)
+
+    # initial odometry pose (pose2d_sparki_odometry) as a new Pose2D message object
+    publisher_odom.publish(Pose2D(0.0, 0.0, 0.0))
+
+    # set servo to an angle pointing inward to the map
+    publisher_servo.publish(45)
+
+
+def callback_update_odometry(data):
+    # Receives geometry_msgs/Pose2D message
+    global pose2d_sparki_odometry = data
+
+
+def callback_update_state(data):
+    # Creates a dictionary object from the JSON string received from the state topic
+    global sensor_dict = json.loads(data.data)
+
+
+def publish_odometry(x, y, theta):
+    publisher_odom.publish(Pose2D(x, y, theta))
+
+
+def convert_ultrasonic_to_robot_coords(x_us):
+    # TODO: Using US sensor reading and servo angle, return value in robot-centric coordinates
+
+    # x_r, y_r = 0.0, 0.0
+
+    # not necessarily relative because it also factors in sparki's theta top make calculation easier
+    x_r = (x_us / 100.0) * cos(sensor_dict['servo'] + pose2d_sparki_odometry.theta)
+    y_r = (x_us / 100.0) * sin(sensor_dict['servo'] + pose2d_sparki_odometry.theta)
+
+    return x_r, y_r
+
+
+def convert_robot_coords_to_world(x_r, y_r):
+    # TODO: Using odometry, convert robot-centric coordinates into world coordinates
+
+    # x_w, y_w = 0.0, 0.0
+
+    x_w = x_r + pose2d_sparki_odometry.x
+    y_w = y_r + pose2d_sparki_odometry.y
+
+    return x_w, y_w
+
+
+def populate_map_from_ping(x_ping, y_ping):
+    # TODO: Given world coordinates of an object detected via ping, fill in the corresponding part of the map
+
+    # convert coords to pos
+    col = floor(x_ping / cell_size_x)
+    row = floor(y_ping / cell_size_y)
+
+    world_grid[row, col] = [255, 255, 255]  # fill in tile
+
+    pass
+
+
+def display_map():
+    # TODO: Display the map
+
+    ani = animation.FuncAnimation(fig, updateFig, interval=50, blit=True)
+    plt.show()
+
+
+pass
+
+
+def ij_to_cell_index(i, j):
+    # TODO: Convert from i,j coordinates to a single integer that identifies a grid cell
+
+    # assumptions:
+    # i is col, j is row ?
+    # i & j are in cm
+
+    # row * num_cols + col
+    cell_num = floor(j / SPARKI_SIZE) * int((N / cell_size_x) / SPARKI_SIZE) + floor(i / SPARKI_SIZE)
+
+    # converts the coordinate system to sparki-size unit cells
+
+    return cell_num
+
+
+def cell_index_to_ij(cell_index):
+    # TODO: Convert from cell_index to (i,j) coordinates
+
+    # i, j = 0.0, 0.0
+
+    i = floor(cell_num * SPARKI_SIZE / int((N / cell_size_x))
+    j = floor(cell_num * SPARKI_SIZE % int((M / cell_size_y))
+
+    # reverse the above function
+
+    return i, j
+
+
+def cost(cell_index_from, cell_index_to):
+    # TODO: Return cost of traversing from one cell to another
+
+    # use a shortest path alg
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+
--- scripts/lab4_refactored.py
+++ scripts/lab4_refactored.py
@@ -6,13 +6,15 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Pose, Pose2D, PoseArray, Point32, PointStamped
+from sensor_msgs.msg import PointCloud, ChannelFloat32
 from std_msgs.msg import Float32MultiArray, Empty, String, Int16
 from math import floor, cos, sin
 
 
 class SparkiMap(object):
     def __init__(self, cell_size_x, cell_size_y, M, N):
+        self.pc_pub = rospy.Publisher('/sparki/obstacle_pc', PoseArray, queue_size=10)
         self.M = M
         self.N = N
         self.cell_size_x = cell_size_x
@@ -24,9 +26,10 @@ class SparkiMap(object):
         self.sparki_x = 0.0
         self.sparki_y = 0.0
 
-        ani = animation.FuncAnimation(self.fig, self.update_fig, interval=50, blit=True)
-        plt.draw()
-
+        #ani = animation.FuncAnimation(self.fig, self.update_fig, interval=50, blit=True)
+        plt.ion()
+        # plt.draw()
+        #plt.pause(0.1)
         plt.show()
 
 
@@ -34,47 +37,74 @@ class SparkiMap(object):
     def update_position(self, x, y):
         self.sparki_x = x
         self.sparki_y = y
+        self.update_fig()
 
-    def update_fig(self, dummy):
-        for j in range(int(self.N / self.cell_size_y)):
-            for i in range(int(self.M / self.cell_size_x)):
-                if ((self.world_grid[i, j] == [0, 0, 0]).all() or (self.world_grid[i, j] == [255, 0, 0]).all()):
-                    if (floor(self.sparki_x) == i and floor(self.sparki_y) == j):
-                        self.world_grid[i, j] = [255, 0, 0]
-                    else:
-                        self.world_grid[i, j] = [0, 0, 0]
-        self.img.set_data(self.world_grid)
-        plt.pause(0.00001)
-        return [self.img]
+    def update_fig(self):
+        # print("here")
+        # for j in range(int(self.N / self.cell_size_y)):
+        #     for i in range(int(self.M / self.cell_size_x)):
+        #         if ((self.world_grid[i, j] == [0, 0, 0]).all() or (self.world_grid[i, j] == [255, 0, 0]).all()):
+        #             if (floor(self.sparki_x) == i and floor(self.sparki_y) == j):
+        #                 self.world_grid[i, j] = [255, 0, 0]
+        #             else:
+        #                 self.world_grid[i, j] = [0, 0, 0]
+        # self.img.set_data(self.world_grid)
+        #
+        # plt.draw()
+        # plt.pause(0.01)
+        # print(self.sparki_x)
+        # print(self.sparki_y)
+        #return [self.img]
+        pc = PoseArray() # type: PoseArray
+        pc.header.stamp = rospy.Time.now()
+        pc.header.frame_id = "map"
+        p1 = Pose()
+        p1.position.x = 1.0
+        p1.position.y = 1.0
+        p1.position.z = 1.0
+        p1.orientation.w = 1
 
-    def show_map(self):
-        plt.imshow(self.world_grid, interpolation='nearest')
-        plt.show()
+        p2 = Pose()
+        p2.position.x = 2.0
+        p2.position.y = 2.0
+        p2.position.z = 1.0
+        p2.orientation.w = 1
+
+        pc.poses = [p1, p2]
+        self.pc_pub.publish(pc)
 
 
 class SparkiController(object):
     IR_THRESHOLD = 300
 
     def __init__(self):
+        # Sparki's Map
+        self.map = SparkiMap(0.01, 0.01, .42, .60)
+
         # ROS Publishers
         self.motor_pub = rospy.Publisher('/sparki/motor_command', Float32MultiArray, queue_size=10)
         self.ping_pub = rospy.Publisher('/sparki/ping_command', Empty, queue_size=10)
         self.odom_pub = rospy.Publisher('/sparki/set_odometry', Pose2D, queue_size=10)
         self.servo_pub = rospy.Publisher('/sparki/set_servo', Int16, queue_size=10)
-
+        self.servo_pub.publish(Int16(45))
+        self.render_pub = rospy.Publisher('/sparki/render_sim', Empty, queue_size=10)
+        self.point_pub = rospy.Publisher('/sparki/point', PointStamped, queue_size=10)
         # ROS Subscribers
         self.odom_sub = rospy.Subscriber('/sparki/odometry', Pose2D, self.update_odom_cb)
         self.state_sub = rospy.Subscriber('/sparki/state', String, self.update_state_cb)
 
         # ROS Timers
-        self.line_follow_tmr = rospy.Timer(rospy.Duration(secs=1), self.line_follow_cb)
+        self.line_follow_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.line_follow_cb)
+        self.render_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.render_cb)
+        self.us_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.update_obstacle)
+
 
         # State Info
         self.present_pose = Pose2D()
         self.sensors_dict = {}
 
-        # Sparki's Map
-        self.map = SparkiMap(0.01, 0.01, .42, .60)
+    def render_cb(self, msg):
+        self.render_pub.publish(Empty())
 
     def update_odom_cb(self, msg):
         """
@@ -83,7 +113,14 @@ class SparkiController(object):
         :return:
         """
         self.present_pose = msg
-        self.map.update_position(x=msg.x, y=msg.y)
+        self.map.update_position(msg.x, msg.y)
+        point = PointStamped()
+        point.header.stamp = rospy.Time.now()
+        point.header.frame_id = "map"
+        point.point.x = msg.x * 5.0
+        point.point.y = msg.y * 5.0
+        point.point.z = 1
+        self.point_pub.publish(point)
 
     def update_state_cb(self, msg):
         """
@@ -91,7 +128,7 @@ class SparkiController(object):
         :param msg:
         :return:
         """
-        self.sensors_dict = json.load(msg.data)
+        self.sensors_dict = json.loads(msg.data)
 
     def line_follow_cb(self, event):
         ir = self.sensors_dict["light_sensors"]
@@ -108,6 +145,14 @@ class SparkiController(object):
             motor_com.data = [0.25, 0.25]
 
         self.motor_pub.publish(motor_com)
+
+    def update_obstacle(self):
+        x_us = self.sensors_dict["ping"]
+        if(x_us < 0):
+            return
+        x_w, y_w = self.robot_to_world(*self.us_to_robot(x_us))
+
+        self.map.add_obstacle(x_w, y_w)
 
     def us_to_robot(self, x_us):
         x_r = (x_us / 100.0) * cos(self.sensors_dict['servo'] + self.present_pose.theta)
