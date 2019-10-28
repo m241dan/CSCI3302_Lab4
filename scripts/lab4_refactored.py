5b34d1e3265ec75eb7c8636617cb8fdcc781fe84
Daniel R. Koris <korisd@gmail.com>
2019-10-28 12:30:15 -0600

    most recent version

--- scripts/lab4_refactored.py
+++ scripts/lab4_refactored.py
@@ -6,75 +6,13 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-from geometry_msgs.msg import Pose, Pose2D, PoseArray, Point32, PointStamped
+from geometry_msgs.msg import Pose2D, TransformStamped, PoseStamped
 from sensor_msgs.msg import PointCloud, ChannelFloat32
 from std_msgs.msg import Float32MultiArray, Empty, String, Int16
 from tf.transformations import quaternion_from_euler
 from math import floor, cos, sin
 from angles import normalize_angle
-
-
-# class SparkiMap(object):
-#     def __init__(self, cell_size_x, cell_size_y, M, N):
-#         self.pc_pub = rospy.Publisher('/sparki/obstacle_pc', PoseArray, queue_size=10)
-#         self.M = M
-#         self.N = N
-#         self.cell_size_x = cell_size_x
-#         self.cell_size_y = cell_size_y
-#         self.world_grid = np.zeros(shape=(int(M / cell_size_x), int(N / cell_size_y), 3))
-#
-#         self.fig = plt.figure()
-#         self.img = plt.imshow(self.world_grid, interpolation='nearest')
-#         self.sparki_x = 0.0
-#         self.sparki_y = 0.0
-#
-#         #ani = animation.FuncAnimation(self.fig, self.update_fig, interval=50, blit=True)
-#         plt.ion()
-#         # plt.draw()
-#         #plt.pause(0.1)
-#         plt.show()
-#
-#
-#
-#     def update_position(self, x, y):
-#         self.sparki_x = x
-#         self.sparki_y = y
-#         self.update_fig()
-#
-#     def update_fig(self):
-#         # print("here")
-#         # for j in range(int(self.N / self.cell_size_y)):
-#         #     for i in range(int(self.M / self.cell_size_x)):
-#         #         if ((self.world_grid[i, j] == [0, 0, 0]).all() or (self.world_grid[i, j] == [255, 0, 0]).all()):
-#         #             if (floor(self.sparki_x) == i and floor(self.sparki_y) == j):
-#         #                 self.world_grid[i, j] = [255, 0, 0]
-#         #             else:
-#         #                 self.world_grid[i, j] = [0, 0, 0]
-#         # self.img.set_data(self.world_grid)
-#         #
-#         # plt.draw()
-#         # plt.pause(0.01)
-#         # print(self.sparki_x)
-#         # print(self.sparki_y)
-#         #return [self.img]
-#         pc = PoseArray() # type: PoseArray
-#         pc.header.stamp = rospy.Time.now()
-#         pc.header.frame_id = "map"
-#         p1 = Pose()
-#         p1.position.x = 1.0
-#         p1.position.y = 1.0
-#         p1.position.z = 1.0
-#         p1.orientation.w = 1
-#
-#         p2 = Pose()
-#         p2.position.x = 2.0
-#         p2.position.y = 2.0
-#         p2.position.z = 1.0
-#         p2.orientation.w = 1
-#
-#         pc.poses = [p1, p2]
-#         self.pc_pub.publish(pc)
-
+import tf2_ros
 
 class SparkiController(object):
     IR_THRESHOLD = 300
@@ -89,23 +27,23 @@ class SparkiController(object):
         self.odom_pub = rospy.Publisher('/sparki/set_odometry', Pose2D, queue_size=10)
         self.servo_pub = rospy.Publisher('/sparki/set_servo', Int16, queue_size=10)
         self.render_pub = rospy.Publisher('/sparki/render_sim', Empty, queue_size=10)
-        self.pose_pub = rospy.Publisher('/sparki/point', Pose2D, queue_size=10)
-        self.pc_pub = rospy.Publisher('/obstacle/point', PointStamped, queue_size=10)
+        self.pose_pub = rospy.Publisher('/sparki/transform', TransformStamped, queue_size=10)
+        self.obstacle_pub = rospy.Publisher('/obstacle/transform', TransformStamped, queue_size=10)
+        self.obstacle_pose = rospy.Publisher('/obstacle/pose', PoseStamped, queue_size=10)
         # ROS Subscribers
         self.odom_sub = rospy.Subscriber('/sparki/odometry', Pose2D, self.update_odom_cb)
         self.state_sub = rospy.Subscriber('/sparki/state', String, self.update_state_cb)
 
         # ROS Timers
-        self.line_follow_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.line_follow_cb)
-        self.render_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.render_cb)
-        self.us_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.update_obstacle)
+        # self.render_tmr = rospy.Timer(rospy.Duration.from_sec(0.1), self.render_cb)
 
         # State Info
         self.present_pose = Pose2D()
         self.sensors_dict = {}
 
-    def render_cb(self, msg):
-        self.render_pub.publish(Empty())
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.buffer)
 
     def update_odom_cb(self, msg):
         """
@@ -116,13 +54,20 @@ class SparkiController(object):
         self.present_pose = msg
         self.present_pose.theta = normalize_angle(self.present_pose.theta)
         # self.map.update_position(msg.x, msg.y)
-        point = PointStamped()
-        point.header.stamp = rospy.Time.now()
-        point.header.frame_id = "map"
-        point.point.x = msg.x
-        point.point.y = msg.y
-        point.point.z = 1
-        self.point_pub.publish(point)
+        pose = TransformStamped()
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = "map"
+        pose.child_frame_id = "sparki"
+        pose.transform.translation.x = msg.x
+        pose.transform.translation.y = msg.y
+        pose.transform.translation.z = 0.0
+        q = quaternion_from_euler(0, 0, self.present_pose.theta)
+        pose.transform.rotation.x = q[0]
+        pose.transform.rotation.y = q[1]
+        pose.transform.rotation.z = q[2]
+        pose.transform.rotation.w = q[3]
+        self.broadcaster.sendTransform(pose)
+        self.pose_pub.publish(pose)
 
     def update_state_cb(self, msg):
         """
@@ -134,9 +79,6 @@ class SparkiController(object):
         # rospy.loginfo(self.sensors_dict)
         if self.sensors_dict["servo"] != 80.0:
             self.servo_pub.publish(Int16(80))
-        self.ping_pub.publish(Empty())
-
-    def line_follow_cb(self, event):
         if "light_sensors" in self.sensors_dict:
             ir = self.sensors_dict["light_sensors"]
             motor_com = Float32MultiArray()
@@ -152,28 +94,45 @@ class SparkiController(object):
                 motor_com.data = [1.00, 1.00]
 
             self.motor_pub.publish(motor_com)
-
-    def update_obstacle(self, event):
         if "ping" in self.sensors_dict:
-            x_us = self.sensors_dict["ping"]
-            if x_us < 0:
-                return
-            x_w, y_w = self.us_to_world(x_us)
-            obstacle = PointStamped()
-            obstacle.header.frame_id = "map"
-            obstacle.header.stamp = rospy.Time.now()
-            obstacle.point.x = x_w
-            obstacle.point.y = y_w
-            obstacle.point.z = 0.0
-            self.pc_pub.publish(obstacle)
+            self.update_obstacle(self.sensors_dict["ping"])
+        self.ping_pub.publish(Empty())
+        self.render_pub.publish(Empty())
+
+    def update_obstacle(self, x_us):
+        if x_us < 0:
+            return
+        # x_w, y_w = self.us_to_world(x_us)
+        obstacle = TransformStamped()
+        obstacle.header.frame_id = "sparki"
+        obstacle.header.stamp = rospy.Time.now()
+        obstacle.child_frame_id = "obstacle"
+        obstacle.transform.translation.x = x_us * cos(np.deg2rad(self.sensors_dict["servo"]))
+        obstacle.transform.translation.y = x_us * sin(np.deg2rad(self.sensors_dict["servo"]))
+        obstacle.transform.rotation.w = 1
+        self.obstacle_pub.publish(obstacle)
+        self.broadcaster.sendTransform(obstacle)
+
+        try:
+            trans = self.buffer.lookup_transform("map", "obstacle", rospy.Time())
+            pose = PoseStamped()
+            pose.header = trans.header
+            pose.pose.position.x = trans.transform.translation.x
+            pose.pose.position.y = trans.transform.translation.y
+            pose.pose.position.z = trans.transform.translation.z
+            self.obstacle_pose.publish(pose)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            return
+
+
 
         # self.map.add_obstacle(x_w, y_w)
 
     def us_to_world(self, x_us):
         x_r = x_us * cos(np.deg2rad(self.sensors_dict['servo']))
         y_r = x_us * sin(np.deg2rad(self.sensors_dict['servo']))
-        x_w = x_r * cos(self.present_pose.theta) + self.present_pose.x
-        y_w = y_r * sin(self.present_pose.theta) + self.present_pose.y
+        x_w = x_r * cos(self.present_pose.theta) - y_r * sin(self.present_pose.theta) + self.present_pose.x
+        y_w = x_r * sin(self.present_pose.theta) + y_r * cos(self.present_pose.theta) + self.present_pose.y
         return x_w, y_w
 
     def cost(self, cell_index_from, cell_index_to):
